53563614aa5d86be03deb5e043fe3bcc9e2db0eb
kartikey92-git <akartikey92@gmail.com>
2026-09-04 16:07:22 +0530

    Add files via upload

---
+++ 51.c
@@ -0,0 +1,30 @@
+/* Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345 */
+
+#include <stdio.h>
+
+int main()
+{
+    int i,k, j;
+
+    for (i = 5; i >= 1; i--)
+    {
+        for (k = 1; k <= i - 1; k++)     
+        {
+            printf(" ");
+        }
+
+        for (j = i; j <= 5; j++)     
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
